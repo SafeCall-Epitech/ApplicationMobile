ddc4e55d8a3f907e8aeff9ed3b421fca2838d4e7
Théau "AimSteyz" Bénézet <theau.benezet@epitech.eu>
2022-10-19 23:48:24 +0200

    Current App
    re-uploading the current used app

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import Navigation from "./Pages/Navigation"
+
+const App = () => {
+  return (
+      <Navigation/>
+    );
+};
+
+export default App;
