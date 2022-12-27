3151004d3bd47b1a481ec0acc0c5bc815e9f5880
Theau Benezet <theau.benezet@epitech.eu>
2022-12-27 15:42:03 +0000

    login page

---
+++ App/Pages/Login.js
@@ -0,0 +1,76 @@
+import React, { useState } from "react";
+import { Text, View, TextInput, StyleSheet, Image } from "react-native";
+import { Button } from "@react-native-material/core";
+
+const Login = ({navigation}) => {
+
+    const [loading, setLoading] = useState(false);
+
+    const [UserName, setUserName] = useState();
+    const [Password, setPassword] = useState();
+    const Myresponse = null;
+
+    function getlogin(UserName, Password) {
+        setLoading(true);
+        const Myresponse = fetch('http://20.234.168.103:8080/login/' + UserName + '/' + Password)
+        .then((response)=>response.json())
+        .then((responseJson) => {
+            console.log(responseJson);
+            setLoading(false);
+            if (responseJson["success"]) {
+                navigation.navigate('Home');
+            } else {
+                alert("Error: Incorrect password or username");
+            }
+        })
+    };
+
+    return (
+        <View style={styles.view}>
+            <Image style={styles.bg} source={require('../img/bg.jpg')} />
+            <Image style={styles.tinyLogo} source={require('../img/logo.png')} />
+            <TextInput placeholder="Username" placeholderTextColor="black" style={styles.input} onChangeText={(UserName) => setUserName(UserName)} value={UserName}/>
+            <TextInput secureTextEntry={true} placeholder="Password" placeholderTextColor="black" style={styles.input} onChangeText={(Password) => setPassword(Password)} value={Password}/>
+            <Button title="login" color="lightgray" style={{marginTop: 20,}} loading={loading} onPress={() => {getlogin(UserName, Password)}}/>
+            <Text style={{padding:25}}>No account yet ?
+                <Text style={styles.registertext} onPress={() => {navigation.navigate('Register')}}> Register you account</Text>
+            </Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    view: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    bg: {
+        flex: 1,
+        resizeMode: 'cover',
+        width: '100%',
+        height: '100%',
+        position: 'absolute',
+        top: 0,
+        left: 0,
+    },
+    tinyLogo: {
+        resizeMode: 'contain',
+        width: '100%',
+    },
+    input: {
+        width: 250,
+        height: 50,
+        margin: 20,
+        borderBottomWidth: 1,
+    },
+    registertext: {
+        color: 'blue',
+        textDecorationLine: "underline",
+        fontFamily: "sans-serif",
+        fontStyle: "italic",
+    }
+
+})
+
+export default Login;
---
+++ App/Pages/Navigation.js
@@ -0,0 +1,27 @@
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Login from "./Login";
+// import Register from "./Register";
+// import HomeScreen from "./HomeScreen";
+// import SplashScreen from "./SplashScreen";
+
+const Stack = createNativeStackNavigator();
+
+const MyStack = () => {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator
+            screenOptions={{
+                headerShown: false
+            }}>
+                {/* <Stack.Screen name="SplashScreen" component={SplashScreen}/> */}
+                <Stack.Screen name="Login" component={Login}/>
+                {/* <Stack.Screen name="Register" component={Register}/> */}
+                {/* <Stack.Screen name="Home" component={HomeScreen}/> */}
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+};
+
+export default MyStack;
