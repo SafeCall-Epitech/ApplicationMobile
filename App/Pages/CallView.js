c2b60072293ac66726d11f1de315983c11832ca9
AimSteyz <theau.benezet@epitech.eu>
2024-01-23 15:47:03 +0100

    FIX CALL THX JULIEN

--- App/Pages/CallView.js
+++ App/Pages/CallView.js
@@ -8,7 +8,14 @@ const MyWebComponent = ({navigation}) => {
   const route = useRoute();
   const CallWith = route.params?.guest;
   const Caller = route.params?.UserName;
-  return <WebView source={{ uri: `https://safecall-web.vercel.app/phonecall/${CallWith}/${Caller}`, javaScriptEnabled: true, DOMdomStorageEnabled: true, originWhitelist: ["https://*"], mixedContentMode: "always",}} style={{ flex: 1 }} />;
+  return (
+    <WebView
+      source={{ uri: `https://safecall-web.vercel.app/phonecall/${CallWith}/${Caller}` }}
+      javaScriptEnabled={true}
+      domStorageEnabled={true}
+      mediaPlaybackRequiresUserAction={false}  // Permet la lecture automatique du son et de la vidéo
+    />
+  );
 }
 
 export default MyWebComponent
