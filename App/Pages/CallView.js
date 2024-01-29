b79e598b89f969f073a864a563fc0863dac45a11
AimSteyz <theau.benezet@epitech.eu>
2024-01-29 21:05:29 +0100

    add hang button on the view

--- App/Pages/CallView.js
+++ App/Pages/CallView.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome5';
 import { WebView } from 'react-native-webview';
 import {useRoute} from '@react-navigation/native';
 
@@ -9,12 +10,35 @@ const MyWebComponent = ({navigation}) => {
   const CallWith = route.params?.guest;
   const Caller = route.params?.UserName;
   return (
-    <WebView
-      source={{ uri: `https://safecall-web.vercel.app/phonecall/${CallWith}/${Caller}` }}
-      javaScriptEnabled={true}
-      domStorageEnabled={true}
-      mediaPlaybackRequiresUserAction={false}  // Permet la lecture automatique du son et de la vidéo
-    />
+    <View style={{ flex: 1 }}>
+      <WebView
+        source={{ uri: `https://safecall-web.vercel.app/phonecall/${CallWith}/${Caller}` }}
+        javaScriptEnabled={true}
+        domStorageEnabled={true}
+        mediaPlaybackRequiresUserAction={false}
+        style={{ flex: 1 }}
+      />
+      <TouchableOpacity
+        style={{
+          position: 'absolute',
+          justifyContent: 'center',
+          alignItems: 'center',
+          width: 100,
+          height: 30,
+          backgroundColor: 'red',
+          borderRadius: 5,
+          // alignSelf: 'center',
+          bottom: 20,
+          right: 20,
+        }}
+        onPress={() => {
+          navigation.navigate('Home');
+        }}
+      >
+         <Icon size={20} name="phone-slash"/>
+        {/* <Text style={{ color: 'white' }}>Your Button</Text> */}
+      </TouchableOpacity>
+    </View>
   );
 }
 
